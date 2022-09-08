7b0425532cfb80b5b873f01bb6899ba3020c3360
hoshicameron <hoshicameron@gmail.com>
2022-09-08 17:11:08 +0430

    7th Commit + PlayerInput Class

---
+++ Assets/Game/Scripts/GamePlay/Player/PlayerInput.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrehistoricPlatformer.Player
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [field:SerializeField]
+        public Vector2 MovementVector { get; private set; }
+        public Action OnAttack, OnJumpPressed, OnJumpReleased, OnWeaponChange;
+        public Action<Vector2> OnMovement;
+        public KeyCode jumpKey, AttackKey,swapWeaponKey, menuKey;
+
+        public UnityEvent OnMenuKeyPressed;
+
+        private void Update()
+        {
+            if (Time.timeScale > 0)
+            {
+                GetMovementInput();
+                GetJumpInput();
+                GetAttackInput();
+                GetWeaponSwapInput();
+            }
+
+            GetMenuInput();
+        }
+
+        private void GetMovementInput()
+        {
+            MovementVector = GetMovementVector();
+            OnMovement?.Invoke(MovementVector);
+        }
+
+        private Vector2 GetMovementVector()
+        {
+            return new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        }
+
+        private void GetJumpInput()
+        {
+            if (Input.GetKeyDown(jumpKey))
+            {
+                OnJumpPressed?.Invoke();
+            }
+
+            if (Input.GetKeyUp(jumpKey))
+            {
+                OnJumpReleased?.Invoke();
+            }
+        }
+
+        private void GetAttackInput()
+        {
+            if (Input.GetKeyDown(AttackKey))
+            {
+                OnAttack?.Invoke();
+            }
+        }
+
+        private void GetWeaponSwapInput()
+        {
+            if (Input.GetKeyDown(swapWeaponKey))
+            {
+                OnWeaponChange?.Invoke();
+            }
+        }
+
+        private void GetMenuInput()
+        {
+            if (Input.GetKeyDown(menuKey))
+            {
+                OnMenuKeyPressed?.Invoke();
+            }
+        }
+    }//class
+}// namespace
