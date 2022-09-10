cb73d976328b3a515bcfe5a7e3df723b14cacd40
hoshicameron <hoshicameron@gmail.com>
2022-09-10 13:42:40 +0430

    16th Commit Implementing State Pattern +State Script

---
+++ Assets/Game/Scripts/GamePlay/StatePattern/State.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PrehistoricPlatformer.StatePattern
+{
+    public abstract class State:MonoBehaviour
+    {
+        protected Agent.Agent agent;
+        public UnityEvent OnEnter, OnExit;
+
+        public void InitializeState(Agent.Agent agent)
+        {
+            this.agent = agent;
+        }
+
+        public void Enter()
+        {
+            agent.agentInput.OnAttack += HandleAttack;
+            agent.agentInput.OnMovement += HandleMovement;
+            agent.agentInput.OnJumpPressed += HandleJumpPressed;
+            agent.agentInput.OnJumpReleased += HandleJumpReleased;
+            agent.agentInput.OnWeaponChange += HandleWeaponChange;
+
+            OnEnter?.Invoke();
+            EnterState();
+
+        }
+
+        protected virtual void EnterState(){}
+        protected virtual void HandleWeaponChange(){}
+        protected virtual void HandleJumpReleased(){}
+
+        protected virtual void HandleJumpPressed(){}
+
+        protected virtual void HandleMovement(Vector2 obj){}
+
+        protected virtual void HandleAttack(){}
+
+        public void Exit()
+        {
+            agent.agentInput.OnAttack -= HandleAttack;
+            agent.agentInput.OnMovement -= HandleMovement;
+            agent.agentInput.OnJumpPressed -= HandleJumpPressed;
+            agent.agentInput.OnJumpReleased -= HandleJumpReleased;
+            agent.agentInput.OnWeaponChange -= HandleWeaponChange;
+
+            OnExit?.Invoke();
+            ExitState();
+        }
+
+        protected virtual void ExitState(){}
+    }
+}
