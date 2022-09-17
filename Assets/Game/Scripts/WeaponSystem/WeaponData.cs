35a3435a38e794983a6f9870bbfedd3fdfb39a70
hoshicameron <hoshicameron@gmail.com>
2022-09-17 14:28:27 +0430

    48th Commit Implement Weapon System +WeaponStorage Functionality

--- Assets/Game/Scripts/WeaponSystem/WeaponData.cs
+++ Assets/Game/Scripts/WeaponSystem/WeaponData.cs
@@ -5,6 +5,7 @@ namespace PrehistoricPlatformer.WeaponSystem
     public class WeaponData
     {
         public Sprite weaponSprite;
+        public string weaponName;
 
         public bool CanBeUsed(bool value)
         {
--- Assets/Game/Scripts/WeaponSystem/WeaponStorage.cs
+++ Assets/Game/Scripts/WeaponSystem/WeaponStorage.cs
@@ -1,29 +1,38 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PrehistoricPlatformer.WeaponSystem
 {
     public class WeaponStorage
     {
+        private List<WeaponData> weaponDataList = new List<WeaponData>();
+        private int currentWeaponIndex = -1;
+
+        public int WeaponCount => weaponDataList.Count;
+
         public WeaponData GetCurrentWeapon()
         {
-            //
+            return currentWeaponIndex == -1 ? null : weaponDataList[currentWeaponIndex];
         }
-
-        public int WeaponCount { get; set; }
-
         public WeaponData SwapWeapon()
         {
-            //
+            if (currentWeaponIndex == -1) return null;
+
+            currentWeaponIndex = (currentWeaponIndex + 1) % (weaponDataList.Count-1);
+            return weaponDataList[currentWeaponIndex];
         }
 
         public void AddWeaponData(WeaponData weaponData)
         {
-            //
+            if(weaponDataList.Contains(weaponData))    return;
+
+            weaponDataList.Add(weaponData);
+            currentWeaponIndex = weaponDataList.Count - 1;
         }
 
         public List<string> GetPlayerWeaponNames()
         {
-            //
+            return weaponDataList.Select(weapon => weapon.weaponName).ToList();
         }
     }
 }
