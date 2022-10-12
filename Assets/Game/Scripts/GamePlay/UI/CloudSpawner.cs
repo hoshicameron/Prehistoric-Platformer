698f2c220b31674aca2875b6c9b5f893cd3cd7fa
hoshicameron <hoshicameron@gmail.com>
2022-10-12 18:29:31 +0330

    83th Commit Implement Level Manager

--- Assets/Game/Scripts/GamePlay/UI/CloudSpawner.cs
+++ Assets/Game/Scripts/GamePlay/UI/CloudSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PrehistoricPlatformer.UI
@@ -5,5 +6,74 @@ namespace PrehistoricPlatformer.UI
     public class CloudSpawner : MonoBehaviour
     {
         
+        [SerializeField]
+        private float width = 1000, length = 1000;
+        [SerializeField]
+        private Color gizmoColor = new Color(1, 0, 0, 0.2f);
+        [SerializeField]
+        private bool showGizmo = true;
+        [SerializeField]
+        private List<Cloud> cloudPrefabs = new List<Cloud>();
+        [SerializeField]
+        private float cloudMinSpeed = 50 , cloudMaxSpeed=70;
+        [SerializeField]
+        private float scaleModifier = 0.5f;
+
+        public Canvas canvas;
+
+
+        private void Start()
+        {
+            foreach (Transform item in transform)
+            {
+                item.GetComponent<Cloud>().Initialize((width / 2) * canvas.scaleFactor + 50, SpawnClouds);
+            }
+        }
+
+        private void SpawnClouds()
+        {
+            Vector3 position = GetRandomPosition();
+
+            int cloudIndex = Random.Range(0, cloudPrefabs.Count);
+            Cloud cloud = cloudPrefabs[cloudIndex];
+
+            float scale = cloud.GetCloudScale() + scaleModifier;
+
+            GameObject cloudObject = Instantiate(cloud.gameObject);
+
+            RectTransform rectTransform = cloudObject.GetComponent<RectTransform>();
+
+            rectTransform.position = position;
+
+            rectTransform.localScale = Vector3.one * scale * canvas.scaleFactor;
+
+            Cloud newCloud = cloudObject.GetComponent<Cloud>();
+
+            newCloud.Speed = Random.Range(cloudMinSpeed,cloudMaxSpeed);
+
+            rectTransform.SetParent(transform);
+
+            newCloud.Initialize(width / 2 * canvas.scaleFactor + 50, SpawnClouds);
+        }
+
+        private Vector3 GetRandomPosition()
+        {
+            return new Vector3(
+                transform.position.x - (width / 2) * canvas.scaleFactor,
+                Random.Range(transform.position.y - length / 2 * canvas.scaleFactor, 
+                    transform.position.y + length / 2 * canvas.scaleFactor), 1);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (showGizmo && canvas != null)
+            {
+                Gizmos.color = gizmoColor;
+                Gizmos.DrawCube(
+                    transform.position, 
+                    new Vector2(width, length) * canvas.scaleFactor);
+
+            }
+        }
     }
 }
---
+++ Assets/Game/Scripts/LevelManagement/LevelManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PrehistoricPlatformer.LevelManagement
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private int level_1BuildIndex, menuBuildIndex, winBuildIndex;
+
+        public void RestartCurrentLevel() => LoadSceneWithIndex(SceneManager.GetActiveScene().buildIndex);
+
+        public void LoadStartLevel() => LoadSceneWithIndex(level_1BuildIndex);
+
+        public void LoadNextLevel() => LoadSceneWithIndex(GetNextLevelIndex());
+
+        public void LoadMenu() => LoadSceneWithIndex(menuBuildIndex);
+        public void LoadWinScene() => LoadSceneWithIndex(winBuildIndex);
+        
+
+        private void LoadSceneWithIndex(int levelIndex)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+        private int GetNextLevelIndex()
+        {
+            int index = SceneManager.GetActiveScene().buildIndex + 1;
+
+            return (index < SceneManager.sceneCountInBuildSettings) ? index : winBuildIndex;
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+            
+#endif
+        }
+    }
+}
